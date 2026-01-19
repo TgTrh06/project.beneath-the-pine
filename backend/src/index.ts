c64b6e3dc4b12bcea013ca57efdcb57e9b44d988
Itsuki06 <trinhtung0604@gmail.com>
2026-01-19 16:10:52 +0700

    doc: split app.ts & index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,6 @@
-import express from 'express';
+import app from './app';
 
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Hello, Beneath the Pine!');
-});
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
